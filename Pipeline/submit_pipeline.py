6f6506fa18b448119d7550f03a4aae9511240bb8
qglht <quentin.guilhot@student-cs.fr>
2023-06-01 15:20:09 +0200

    removed files

--- Pipeline/submit_pipeline.py
+++
@@ -1,8 +0,0 @@
-from submission import submit
-
-class SubmissionPipeline:
-    def __init__(self, preds):
-        self.preds = preds
-
-    def execute(self):
-        submit(self.preds)
--- submission_task.py
+++
@@ -1,9 +0,0 @@
-import numpy as  np 
-from pipeline import SubmissionPipeline
-
-# load P and Q from P.npy and Q.npy
-if __name__ == "__main__":
-    P = np.load("P.npy")
-    Q = np.load("Q.npy")
-    preds = np.dot(P.T, Q)
-    SubmissionPipeline(preds).execute()
