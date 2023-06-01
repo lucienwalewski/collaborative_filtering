149238fa1e2538adfa5d1764ba80d2024804bb2c
qglht <quentin.guilhot@student-cs.fr>
2023-06-01 11:52:24 +0200

    tasks

---
+++ submission/submit.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def submit(preds):
+
+    # Load the sample submission file
+    submission = pd.read_csv('../data/sampleSubmission.csv')
+
+    # Extract the row and column indices from the 'Id' column
+    submission['row_idx'] = submission['Id'].str.extract(r'r(\d+)_c\d+').astype(int) - 1
+    submission['col_idx'] = submission['Id'].str.extract(r'r\d+_c(\d+)').astype(int) - 1
+
+    # Update the predictions in the submission file
+    submission['Prediction'] = preds[submission['row_idx'], submission['col_idx']]
+
+    # Drop the row and column indices
+    submission.drop(['row_idx', 'col_idx'], axis=1, inplace=True)
+
+    # Save the updated submission file
+    submission.to_csv('../data/mySubmission.csv', index=False)
---
+++ tasks/training_task.py
@@ -0,0 +1,39 @@
+from pipeline import TrainingPipeline
+from models import ALS, MLP
+from data_processing import Data
+
+import argparse
+
+def get_args():
+    args_parser = argparse.ArgumentParser()
+
+    # Experiment arguments
+    args_parser.add_argument(
+        '--dataset',
+        help="""path to dataset""",
+        default="data/data_train.csv")
+    args_parser.add_argument(
+        '--epochs',
+        help="""how many epochs to train for""",
+        default=200,
+        type=int)
+    args_parser.add_argument(
+        '--lmdba',
+        help="""Regularization parameter""",
+        default=0.1,
+        type=int)
+    args_parser.add_argument(
+        '--k',
+        help="""Number of latent factors""",
+        default=10,
+        type=int)
+    
+    return args_parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_args()
+    data = Data(args.dataset)
+    model = ALS(lmbda=args.lmbda, k=args.lmbda, n_epochs=args.epochs)
+    pipeline = TrainingPipeline(data, model)
+    pipeline.execute()
+    print("Model trained and saved")
